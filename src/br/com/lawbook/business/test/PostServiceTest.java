9b0475e6c3169b8b18a52670786c963f44042df7
Junior Ales <sela-roinuj@hotmail.com>
2011-09-15 17:17:39 -0300

    Hibernate tests

--- src/br/com/lawbook/business/test/PostServiceTest.java
+++ src/br/com/lawbook/business/test/PostServiceTest.java
@@ -2,6 +2,8 @@ package br.com.lawbook.business.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import br.com.lawbook.business.PostService;
@@ -12,7 +14,14 @@ public class PostServiceTest {
 	@Test
 	public void getStreamTest() {
 		PostService service = PostService.getInstance();
-		assertNotNull(service.getStream(new Profile().getFriendsList()));
+		Profile profile1 = new Profile();
+		assertNull(service.getStream(profile1.getFriendsList()));
+		
+		Profile profile2 = new Profile();
+		ArrayList<Profile> friendList = new ArrayList<Profile>();
+		friendList.add(profile2);
+		profile1.setFriendsList(friendList);
+		assertNotNull(service.getStream(profile1.getFriendsList()));
 	}
 	
 }
--- src/br/com/lawbook/util/TablesGenerator.java
+++ src/br/com/lawbook/util/TablesGenerator.java
@@ -1,9 +1,13 @@
 package br.com.lawbook.util;
 
-import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
+import java.util.ArrayList;
+import java.util.List;
 
-import br.com.lawbook.model.*;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import br.com.lawbook.model.User;
 
 /**
  * @author Edilson Luiz Ales Junior
@@ -14,14 +18,14 @@ import br.com.lawbook.model.*;
 public class TablesGenerator {
 
 	public static void main(String[] args) {
-		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-		cfg.addAnnotatedClass(User.class);
-		cfg.addAnnotatedClass(Profile.class);
-		cfg.addAnnotatedClass(Location.class);
-		cfg.addAnnotatedClass(Post.class);
-		cfg.addAnnotatedClass(Comment.class);
-		SchemaExport se = new SchemaExport(cfg);
-		se.create(true, true);
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+//		cfg.addAnnotatedClass(User.class);
+//		cfg.addAnnotatedClass(Profile.class);
+//		cfg.addAnnotatedClass(Location.class);
+//		cfg.addAnnotatedClass(Post.class);
+//		cfg.addAnnotatedClass(Comment.class);
+//		SchemaExport se = new SchemaExport(cfg);
+//		se.create(true, true);
 		
 ////		User u;
 ////		for (int i = 0; i < 10; i++) {
@@ -38,20 +42,24 @@ public class TablesGenerator {
 ////			 System.out.println(u.getUserName());
 ////		 }
 //		
-//		Session session = HibernateUtil.getSession();
-//		Transaction tx = session.beginTransaction();
-//		List<String> userNames = new ArrayList<String>();
-//		userNames.add("userName2");
-//		userNames.add("userName0");
-//		userNames.add("userName7");
-//		Query query = session.createQuery("from lwb_user where userName in (:userNames)");
-//		query.setParameter("userNames", userNames);
-//		List<User> users = query.list();
-//		 for (User u : users) {
-//			 System.out.println(u.getUserName());
-//		 }
-//		tx.commit();
-//		session.close();
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		List<String> userNames = new ArrayList<String>();
+		userNames.add("userName2");
+		userNames.add("userName0");
+		userNames.add("userName7");
+		Query query = session.createQuery("from lwb_user u where u.userName = :userName1 or u.userName = :userName2");
+		query.setParameter("userName1", userNames.get(0));
+		query.setParameter("userName2", userNames.get(1));
+		
+		@SuppressWarnings("unchecked")
+		List<User> users = query.list();
+		
+		 for (User u : users) {
+			 System.out.println(u.getUserName());
+		 }
+		tx.commit();
+		session.close();
 	}
 }
 
