fdd47ea913368a888b212c21008af81a70247d37
Jr Ales <sela-roinuj@hotmail.com>
2011-10-26 23:24:06 -0200

    Added security check to avoid running 'TablesGenerator.java' by mistake

--- src/br/com/lawbook/util/TablesGenerator.java
+++ src/br/com/lawbook/util/TablesGenerator.java
@@ -1,18 +1,34 @@
 package br.com.lawbook.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.springframework.security.ldap.userdetails.Person;
 
 import br.com.lawbook.model.*;
 
 /**
  * @author Edilson Luiz Ales Junior
- * @version 25OCT2011-07
+ * @version 26OCT2011-08
  * 
  */
 public class TablesGenerator {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Are you sure you want to do that? All database will be purged! ( Y | N )");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String answer = "";
+		try {
+			answer = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if (!answer.equalsIgnoreCase("y")) return;
 		
 		try {
 			Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
