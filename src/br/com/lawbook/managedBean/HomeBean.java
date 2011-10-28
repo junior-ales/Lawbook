4b70af15a4f4a6829b2769e485a01c6a96e6ede2
Jr Ales <sela-roinuj@hotmail.com>
2011-10-28 01:05:20 -0200

    Re-organization of web pages directories and start to test posting

--- src/br/com/lawbook/managedBean/HomeBean.java
+++ src/br/com/lawbook/managedBean/HomeBean.java
@@ -1,13 +1,15 @@
 package br.com.lawbook.managedBean;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 
 import org.primefaces.model.LazyDataModel;
 import org.springframework.security.core.Authentication;
@@ -18,39 +20,49 @@ import br.com.lawbook.business.PostService;
 import br.com.lawbook.business.ProfileService;
 import br.com.lawbook.model.Post;
 import br.com.lawbook.model.Profile;
+import br.com.lawbook.util.FacesUtil;
 
 /**
  * @author Edilson Luiz Ales Junior
- * @version 24OCT2011-03
+ * @version 27OCT2011-04
  */
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class HomeBean implements Serializable {
 	
 	private Profile profile;
+	private Profile public_;
+	private Post post;
 	private LazyDataModel<Post> stream;
-	private ProfileService service;
+	private ProfileService profileService;
 	private PostService postService;
 	private static final long serialVersionUID = 928727904018740163L;
 
 	public HomeBean() {
-		this.service = ProfileService.getInstance();
+		this.profileService = ProfileService.getInstance();
+		this.postService = PostService.getInstance();
+		this.post = new Post();
+		this.public_ = this.profileService.getProfileByUserName("public");
 		Profile profile = new Profile();
         SecurityContext context = SecurityContextHolder.getContext();
         if (context instanceof SecurityContext){
             Authentication authentication = context.getAuthentication();
             if (authentication instanceof Authentication){
             	String username = ((org.springframework.security.core.userdetails.User)authentication.getPrincipal()).getUsername();
-				profile = this.service.getProfileByUserName(username);
+				profile = this.profileService.getProfileByUserName(username);
             }
         }
     	setProfile(profile);
 	}
 	
+	public LazyDataModel<Post> getStream() {
+		return this.stream;
+	}
+	
 	@PostConstruct
 	public void loadLazyStream() {
 		if (this.stream == null) {
-			this.service = ProfileService.getInstance();
+			this.profileService = ProfileService.getInstance();
 			this.postService = PostService.getInstance();
 			this.stream = new LazyDataModel<Post>() {
 				private static final long serialVersionUID = -4238038748234463347L;
@@ -69,8 +81,20 @@ public class HomeBean implements Serializable {
 		}
 	}
 	
-	public LazyDataModel<Post> getStream() {
-		return this.stream;
+	public void savePost(ActionEvent event) {
+		this.post.setDateTime(Calendar.getInstance());
+		this.post.setSender(this.profile);
+		this.post.setReceiver(this.public_);
+		this.postService.save(this.post);
+		FacesUtil.successMessage("", "Posted");
+	}
+	
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
 	}
 
 	public Profile getProfile() {
---
+++ src/br/com/lawbook/util/FacesUtil.java
@@ -0,0 +1,35 @@
+package br.com.lawbook.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ * @author Edilson Luiz Ales Junior
+ * @version 27OUT2011-01
+ * 
+ */
+public class FacesUtil {
+
+	public static void successMessage(String summary, String message) {
+		showMessage(FacesMessage.SEVERITY_INFO, summary, message);
+	}
+
+	public static void warnMessage(String summary, String message) {
+		showMessage(FacesMessage.SEVERITY_WARN, summary, message);
+	}
+
+	public static void errorMessage(String summary, String message) {
+		showMessage(FacesMessage.SEVERITY_ERROR, summary, message);
+	}
+
+	private static void showMessage(FacesMessage.Severity severity,	String summary, String message) {
+		FacesMessage facesMessage = new FacesMessage(severity, summary, message);
+		FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+	}
+
+	public static ExternalContext getExternalContext() {
+		return FacesContext.getCurrentInstance().getExternalContext();
+	}
+
+}
