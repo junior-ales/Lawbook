1b02d46c7b61ddba0761f5983df9b36dd0fa893b
Jr Ales <sela-roinuj@hotmail.com>
2011-11-26 03:37:03 -0200

    Settings page implemented

--- src/br/com/lawbook/managedbean/ProfileBean.java
+++ src/br/com/lawbook/managedbean/ProfileBean.java
@@ -1,8 +1,12 @@
 package br.com.lawbook.managedbean;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -20,7 +24,7 @@ import br.com.lawbook.util.FacesUtil;
 
 /**
  * @author Edilson Luiz Ales Junior
- * @version 18NOV2011-11
+ * @version 26NOV2011-12
  */
 @ManagedBean
 @SessionScoped
@@ -28,16 +32,21 @@ public class ProfileBean implements Serializable {
 	
 	private Profile authProfile;
 	private Profile profileOwner;
+	private String birth;
 	private LazyDataModel<Post> wall;
-	private ProfileService profileService;
-	private PostService postService;
-	private static final long serialVersionUID = 4305913075799591127L;
+	private static final ProfileService PROFILE_SERVICE = ProfileService.getInstance();
+	private static final PostService POST_SERVICE = PostService.getInstance();
+	private static final Logger LOG = Logger.getLogger("ProfileBean");
+	private static final long serialVersionUID = -7128108390695306743L;
 
 	public ProfileBean() {
 		this.profileOwner = new Profile();
-		this.profileService = ProfileService.getInstance();
 		try {
-			this.setAuthProfile(this.profileService.getAuthorizedUserProfile());
+			this.setAuthProfile(PROFILE_SERVICE.getAuthorizedUserProfile());
+			if (this.authProfile.getBirth() != null) {
+				final SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+				this.birth = df.format(this.authProfile.getBirth().getTime());
+			}
 		} catch (Exception e) {
 			FacesUtil.errorMessage("Authentication Error", "Problem with authentication process: " + e.getMessage());
 		}
@@ -46,18 +55,16 @@ public class ProfileBean implements Serializable {
 	@PostConstruct
 	public void loadLazyWall() {
 		if (this.wall == null) {
-			this.profileService = ProfileService.getInstance();
-			this.postService = PostService.getInstance();
 			this.wall = new LazyDataModel<Post>() {
 				private static final long serialVersionUID = -4238038748234463347L;
 
 				@Override
 				public List<Post> load(int first, int pageSize, String sortField, boolean sortOrder, Map<String, String> filters) {
-					List<Post> posts = postService.getWall(profileOwner, authProfile, first, pageSize);
+					List<Post> posts = POST_SERVICE.getWall(profileOwner, authProfile, first, pageSize);
 					return posts;
 				}
 			};
-			this.wall.setRowCount(this.postService.getPostsCount());
+			this.wall.setRowCount(POST_SERVICE.getPostsCount());
 		}
 	}
 	
@@ -68,7 +75,7 @@ public class ProfileBean implements Serializable {
 		}
 		Post post = (Post) this.wall.getRowData();
 		try {
-			this.postService.delete(post);
+			POST_SERVICE.delete(post);
 			FacesUtil.infoMessage("=)", "Post deleted!");
 		} catch (IllegalArgumentException e) {
 			FacesUtil.warnMessage("=|", e.getMessage());
@@ -79,7 +86,7 @@ public class ProfileBean implements Serializable {
 	
 	public void addFriend(ActionEvent event) {
 		try {
-			this.profileService.friendship(this.authProfile, this.profileOwner);
+			PROFILE_SERVICE.friendship(this.authProfile, this.profileOwner);
 			FacesUtil.infoMessage("=)", "Connection successfully added!");
 		} catch (IllegalArgumentException e) {
 			FacesUtil.warnMessage("=|", e.getMessage());
@@ -105,6 +112,30 @@ public class ProfileBean implements Serializable {
 		return locale;
 	}
 	
+	public void updateProfile() {
+		LOG.info("#### updateProfile()");
+		try {
+			this.authProfile.setBirth(getDate(this.birth));
+			PROFILE_SERVICE.update(this.authProfile);
+			FacesUtil.infoMessage("=)", "Profile updated successfully");
+		} catch (final IllegalArgumentException e) {
+			FacesUtil.warnMessage("=|", e.getMessage());
+		} catch (final HibernateException e) {
+			FacesUtil.errorMessage("=(", e.getMessage());
+		}
+	}
+	
+	private static Calendar getDate(final String dateString) {
+		final Calendar c = Calendar.getInstance();
+		final SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			c.setTime(df.parse(dateString));
+		} catch (final ParseException e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
 	public LazyDataModel<Post> getWall() {
 		return this.wall;
 	}
@@ -123,6 +154,14 @@ public class ProfileBean implements Serializable {
 
 	public void setProfileOwner(Profile profileOwner) {
 		this.profileOwner = profileOwner;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
 	}
 	
 }
